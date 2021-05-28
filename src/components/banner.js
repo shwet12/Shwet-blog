c553f503b1dc010edc69c3101c8b17a708c3c100
Shwet anand <shwetanand2@gmail.com>
2021-05-28 23:24:26 +0530

    added typing effect

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
 import { Link } from 'gatsby'
+import ReactTypingEffect from 'react-typing-effect';
 
 const Banner = () => {
     return (
         <div className="card">
             <div className="container">
                 <h3 className="bannerTitle">Hi folks!</h3>
-                <p className="bannerParagraph">Welcome to my Home.
-                I build things for the web.</p>
+                <ReactTypingEffect
+                    text={["Welcome to my Home.", "I build things for the web."]}
+                    eraseDelay={1000}
+                    typingDelay={1000}
+                />
                 <p className="bannerParagraph">
                 </p>
 
